07e7b475dfc07863fa321e2cf69970d1a34312e2
manhlamabc123 <tranducmanh12092001@gmail.com>
2023-05-12 19:04:51 +0700

    Create template API for input/output service

--- input_output_handling/input_output_handling.py
+++ input_output_handling/input_output_handling.py
@@ -1,11 +1,59 @@
-from flask import Flask
+from flask import Flask, request
 import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/api/template', methods=['POST'])
+def template():
+    # Get request_data from main.py
+    request_data = request.get_json()
+    print(request_data)
+
+    # Handling message
+    '''
+        {
+            "id": string,
+            "features": .csv file,
+            "link_commit": string,
+            "ensemble": boolean,
+            "deep_models": list of string,
+            "traditional_models": list of string,
+            "number_models": int,
+            "metric": list of string
+        }
+    '''
+
+    # Data Preprocessing
+    # Must return a model_request like below:
+    '''
+        {
+            "id": string,
+            "number_models": int,
+            "vectorized_feature": vectorized_feature
+            "metrics": list of string
+        }
+    '''
+
+    # Forward to model
+    # for model in request_data["model"]:
+    #     model_response = requests.post(f'http://localhost:5000/api/{model}', json=model_request.get_json())
+    #     if model_response.status_code == 200:
+    #         print(model_response.json())
+    #     else:
+    #         print('Error:', model_response.status_code)
+
+    # Create response like form below:
+    '''
+        {
+            "id": string,
+            "output": dictionary
+        }
+    '''
+    output = {'auc': 0}
+    return {
+        'id': request_data['id'],
+        'output': output
+    }
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
