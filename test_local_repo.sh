25d4d0c334a41216b601edb93a982a0859faf1ef
npt-1707 <npthanh132@gmail.com>
2023-06-17 00:10:20 +0700

    add auto extract feature

---
+++ test_local_repo.sh
@@ -0,0 +1,7 @@
+python main.py \
+    -debug \
+    -deep cc2vec deepjit \
+    -traditional lapredict\
+    -repo "Tic-tac-toe-Game-using-Network-Socket-APIs" \
+    -commit_hash daea85852200e8c1bfc22707924ac4c53301a1eb \
+    -main_language C
