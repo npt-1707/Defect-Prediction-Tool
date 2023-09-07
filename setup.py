07d7e855289d5788a6e63a99ade2487961a3f9ce
Jirachiii <trangdy1112@gmail.com>
2023-09-07 23:01:24 +0700

    Create a testing version of pip app

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.1'
+DESCRIPTION = 'A cutting-edge defect prediction tool with up-to-date Just-in-Time techniques and a robust API, empowering software development teams to proactively identify and mitigate defects in real-time'
+
+# Setting up
+setup(
+    name="defectguard",
+    version=VERSION,
+    author="NeuralNine (Florian Dedov)",
+    author_email="<mail@neuralnine.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['numpy', 'pandas', 'PyGithub', 'Requests', 'tqdm'],
+    keywords=['python', 'defect', 'prediction', 'just-in-time', 'defect prediction'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
