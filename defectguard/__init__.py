12f98c8a7afa23cf918e1c89fec3fa15a1506d83
manhlamabc <tranducmanh12092001@gmail.com>
2023-09-18 23:48:28 +0700

    Add JITLine to DefectGuard

--- defectguard/__init__.py
+++ defectguard/__init__.py
@@ -4,6 +4,7 @@ from .cc2vec.handler import CC2Vec
 from .simcom.handler import SimCom
 from .lapredict.handler import LAPredict
 from .tlel.handler import TLEL
+from .jitline.handler import JITLine
 # Extractor
 from .auto_extract.RepositoryExtractor import RepositoryExtractor
 # EARL
