15de79592253e35ce02c3e2e1492f432fc07c3b9
manhlamabc123 <tranducmanh12092001@gmail.com>
2023-12-10 16:31:21 +0700

    Update test suits

--- test_suits/test_local_repo.sh
+++ test_suits/test_local_repo.sh
@@ -3,6 +3,6 @@
 defectguard \
     -models deepjit \
     -dataset platform \
-    -repo . \
+    -repo /home/manh/Documents/DefectGuard/Tic-tac-toe-Game-using-Network-Socket-APIs \
     -commit_hash 9cd64889990fd91e0396495f622e536d02e7bf88 061b5c1daf3363f5a88493cba4c6f7e06c934fea \
-    -main_language Python
+    -main_language C
