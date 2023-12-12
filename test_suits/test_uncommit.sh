426de38684e37dd04e7d2bad923e1cdd44658564
manhlamabc123 <tranducmanh12092001@gmail.com>
2023-12-12 18:11:38 +0700

    Update test case for uncommit

--- test_suits/test_uncommit.sh
+++ test_suits/test_uncommit.sh
@@ -4,5 +4,5 @@ defectguard \
     -models deepjit \
     -dataset platform \
     -repo /home/manh/Documents/DefectGuard/Tic-tac-toe-Game-using-Network-Socket-APIs \
-    -commit_hash uncommit dedbc4ed5b953ac03854c83e0b91dd56f4fd1f1e e07e944afd5435a367c8b1789cda20d7c52240c3 c02ebd96206ea49a172c49cf6ee47ce6e9b7cea6 5b55c88d5809bb75db413247eeb7ab9aac94ea46 \
+    -commit_hash uncommit \
     -main_language C
