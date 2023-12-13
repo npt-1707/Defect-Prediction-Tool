fb529ef09f1df956431fc7860e305bff86b1a94d
manhlamabc123 <tranducmanh12092001@gmail.com>
2023-12-13 14:37:37 +0700

    Create setup script

---
+++ test_suits/setup.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+
+cd ..
+git clone https://github.com/manhlamabc123/Tic-tac-toe-Game-using-Network-Socket-APIs
+cd DefectGuard-the-Package
+python setup.py develop
+pip show defectguard
--- test_suits/test_uncommit.sh
+++ test_suits/test_uncommit.sh
@@ -3,6 +3,6 @@
 defectguard \
     -models deepjit \
     -dataset platform \
-    -repo /home/manh/Documents/DefectGuard/Tic-tac-toe-Game-using-Network-Socket-APIs \
+    -repo ../Tic-tac-toe-Game-using-Network-Socket-APIs \
     -commit_hash uncommit \
     -main_language C
